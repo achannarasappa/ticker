1e7e10b2f9823155678a8c53741ed0ae80c83088
Ani Channarasappa <git@ani.dev>
2020-12-27 13:13:49 -0500

    Removed experimental code

--- cmd/main.go
+++ cmd/main.go
@@ -9,12 +9,6 @@ import (
 )
 
 func main() {
-	// fmt.Printf("%#v \n", quote.GetQuotes([]string{"ABNB", "PLTR"}))
-
-	// quotes := []quote.Quote{
-	// 	{Symbol: "AAPL", ShortName: "Apple, Inc.", RegularMarketPrice: 1000.1, RegularMarketChange: 10.1, RegularMarketChangePercent: 1.1},
-	// 	{Symbol: "ABNB", ShortName: "AirBnB, Inc.", RegularMarketPrice: 645.1, RegularMarketChange: 4.1, RegularMarketChangePercent: 0.9},
-	// }
 
 	symbols := []string{
 		"NET",

ad894b5518ba0d331bbe5b8d46d8d82ebaed4363
Ani Channarasappa <git@ani.dev>
2020-12-27 13:39:46 -0500

    Added correct coloring based of change

--- internal/ui/component/watchlist/watchlist.go
+++ internal/ui/component/watchlist/watchlist.go
@@ -12,7 +12,8 @@ import (
 
 var (
 	styleNeutral       = util.NewStyle("#d4d4d4", "", false)
-	styleNeutralFaded  = util.NewStyle("#7e8087", "", false)
+	styleNeutralBold   = util.NewStyle("#d4d4d4", "", true)
+	styleNeutralFaded  = util.NewStyle("#555761", "", false)
 	stylePricePositive = util.NewStyle("#d1ff82", "", false)
 	stylePriceNegative = util.NewStyle("#ff8c82", "", false)
 )
@@ -34,10 +35,10 @@ func NewModel() Model {
 }
 
 func (m Model) View() string {
-	return quoteSummaries(m.Quotes, m.Width)
+	return watchlist(m.Quotes, m.Width)
 }
 
-func quoteSummaries(q []quote.Quote, elementWidth int) string {
+func watchlist(q []quote.Quote, elementWidth int) string {
 	quoteSummaries := ""
 	for _, quote := range q {
 		quoteSummaries = quoteSummaries + "\n" + quoteSummary(quote, elementWidth)
@@ -48,23 +49,39 @@ func quoteSummaries(q []quote.Quote, elementWidth int) string {
 func quoteSummary(q quote.Quote, elementWidth int) string {
 
 	firstLine := lineWithGap(
-		styleNeutral(q.Symbol),
+		styleNeutralBold(q.Symbol),
 		styleNeutral(convertFloatToString(q.RegularMarketPrice)),
 		elementWidth,
 	)
 	secondLine := lineWithGap(
 		styleNeutralFaded(q.ShortName),
-		stylePricePositive("↑ "+convertFloatToString(q.RegularMarketChange)+" ("+convertFloatToString(q.RegularMarketChangePercent)+"%)"),
+		priceText(q.RegularMarketChange, q.RegularMarketChangePercent),
 		elementWidth,
 	)
 
 	return fmt.Sprintf("%s\n%s", firstLine, secondLine)
 }
 
+func priceText(change float64, changePercent float64) string {
+	if change == 0.0 {
+		return styleNeutral("  " + convertFloatToString(change) + "  (" + convertFloatToString(changePercent) + "%)")
+	}
+
+	if change > 0.0 {
+		return stylePricePositive("↑ " + convertFloatToString(change) + "  (" + convertFloatToString(changePercent) + "%)")
+	}
+
+	return stylePriceNegative("↓ " + convertFloatToString(change) + " (" + convertFloatToString(changePercent) + "%)")
+}
+
 // util
 func lineWithGap(leftText string, rightText string, elementWidth int) string {
 	innerGapWidth := elementWidth - ansi.PrintableRuneWidth(leftText) - ansi.PrintableRuneWidth(rightText)
-	return leftText + strings.Repeat(" ", innerGapWidth) + rightText
+	if innerGapWidth > 0 {
+		return leftText + strings.Repeat(" ", innerGapWidth) + rightText
+	}
+
+	return leftText + " " + rightText
 }
 
 func convertFloatToString(f float64) string {

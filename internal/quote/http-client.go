a0948aab2be0efeba3ac642a4a463073964ca802
Ani Channarasappa <git@ani.dev>
2020-12-27 16:20:49 -0500

    Added market state price and price color gradient

--- internal/quote/http-client.go
+++ internal/quote/http-client.go
@@ -9,17 +9,15 @@ import (
 
 type Quote struct {
 	Currency                   string  `json:"currency"`
-	MarketCap                  int64   `json:"marketCap"`
 	ShortName                  string  `json:"shortName"`
+	Symbol                     string  `json:"symbol"`
 	MarketState                string  `json:"marketState"`
 	RegularMarketChange        float64 `json:"regularMarketChange"`
 	RegularMarketChangePercent float64 `json:"regularMarketChangePercent"`
 	RegularMarketPrice         float64 `json:"regularMarketPrice"`
-	RegularMarketDayHigh       float64 `json:"regularMarketDayHigh"`
-	RegularMarketDayLow        float64 `json:"regularMarketDayLow"`
-	RegularMarketOpen          float64 `json:"regularMarketOpen"`
-	Symbol                     string  `json:"symbol"`
-	RegularMarketVolume        int     `json:"regularMarketVolume"`
+	PostMarketChange           float64 `json:"postMarketChange"`
+	PostMarketChangePercent    float64 `json:"postMarketChangePercent"`
+	PostMarketPrice            float64 `json:"postMarketPrice"`
 }
 
 type Response struct {
--- internal/ui/component/watchlist/watchlist.go
+++ internal/ui/component/watchlist/watchlist.go
@@ -2,24 +2,32 @@ package watchlist
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 	"ticker-tape/internal/quote"
 	"ticker-tape/internal/ui/util"
 
+	"github.com/lucasb-eyer/go-colorful"
 	"github.com/muesli/reflow/ansi"
 )
 
 var (
 	styleNeutral       = util.NewStyle("#d4d4d4", "", false)
 	styleNeutralBold   = util.NewStyle("#d4d4d4", "", true)
-	styleNeutralFaded  = util.NewStyle("#555761", "", false)
-	stylePricePositive = util.NewStyle("#d1ff82", "", false)
-	stylePriceNegative = util.NewStyle("#ff8c82", "", false)
+	styleNeutralFaded  = util.NewStyle("#616161", "", false)
+	stylePricePositive = newStyleFromGradient("#cae891", "#adff00")
+	stylePriceNegative = newStyleFromGradient("#f5a07d", "#ff5200")
 )
 
 const (
-	footerHeight = 1
+	maxPercentChangeColorGradient = 10
+)
+
+const (
+	PositivePriceChange = iota
+	NegativePriceChange
+	NeutralPriceChange
 )
 
 type Model struct {
@@ -48,30 +56,64 @@ func watchlist(q []quote.Quote, elementWidth int) string {
 
 func quoteSummary(q quote.Quote, elementWidth int) string {
 
+	p := getPrice(q)
+
 	firstLine := lineWithGap(
 		styleNeutralBold(q.Symbol),
-		styleNeutral(convertFloatToString(q.RegularMarketPrice)),
+		styleNeutral(convertFloatToString(p.Price)),
 		elementWidth,
 	)
 	secondLine := lineWithGap(
 		styleNeutralFaded(q.ShortName),
-		priceText(q.RegularMarketChange, q.RegularMarketChangePercent),
+		priceText(p.Change, p.ChangePercent),
 		elementWidth,
 	)
 
 	return fmt.Sprintf("%s\n%s", firstLine, secondLine)
 }
 
+type quoteMeta struct {
+	Price         float64
+	Change        float64
+	ChangePercent float64
+}
+
+func getPrice(q quote.Quote) quoteMeta {
+
+	if q.MarketState == "REGULAR" {
+		return quoteMeta{
+			Price:         q.RegularMarketPrice,
+			Change:        q.RegularMarketChange,
+			ChangePercent: q.RegularMarketChangePercent,
+		}
+	}
+
+	if q.MarketState == "POST" {
+		return quoteMeta{
+			Price:         q.PostMarketPrice,
+			Change:        q.PostMarketChange,
+			ChangePercent: q.PostMarketChangePercent,
+		}
+	}
+
+	return quoteMeta{
+		Price:         q.RegularMarketPrice,
+		Change:        0.0,
+		ChangePercent: 0.0,
+	}
+
+}
+
 func priceText(change float64, changePercent float64) string {
 	if change == 0.0 {
-		return styleNeutral("  " + convertFloatToString(change) + "  (" + convertFloatToString(changePercent) + "%)")
+		return styleNeutralFaded("  " + convertFloatToString(change) + "  (" + convertFloatToString(changePercent) + "%)")
 	}
 
 	if change > 0.0 {
-		return stylePricePositive("↑ " + convertFloatToString(change) + "  (" + convertFloatToString(changePercent) + "%)")
+		return stylePricePositive(change)("↑ " + convertFloatToString(change) + "  (" + convertFloatToString(changePercent) + "%)")
 	}
 
-	return stylePriceNegative("↓ " + convertFloatToString(change) + " (" + convertFloatToString(changePercent) + "%)")
+	return stylePriceNegative(change)("↓ " + convertFloatToString(change) + " (" + convertFloatToString(changePercent) + "%)")
 }
 
 // util
@@ -86,4 +128,36 @@ func lineWithGap(leftText string, rightText string, elementWidth int) string {
 
 func convertFloatToString(f float64) string {
 	return strconv.FormatFloat(f, 'f', 2, 64)
+}
+
+func newStyleFromGradient(startColorHex string, endColorHex string) func(float64) func(string) string {
+	c1, _ := colorful.Hex(startColorHex)
+	c2, _ := colorful.Hex(endColorHex)
+
+	return func(percent float64) func(string) string {
+		normalizedPercent := getNormalizedPercentWithMax(percent, maxPercentChangeColorGradient)
+		return util.NewStyle(c1.BlendHsv(c2, normalizedPercent).Hex(), "", false)
+	}
+}
+
+// Normalize 0-100 percent with a maximum percent value
+func getNormalizedPercentWithMax(percent float64, maxPercent float64) float64 {
+
+	absolutePercent := math.Abs(percent)
+	if absolutePercent > maxPercent {
+		return 1
+	}
+
+	return percent / maxPercent
+
+}
+
+// Accepts a start color, end color, and percent and returns the color at the position between them
+func getColorHex(startColorHex string, endColorHex string, normalizedPercent float64) string {
+
+	c1, _ := colorful.Hex(startColorHex)
+	c2, _ := colorful.Hex(endColorHex)
+
+	return c1.BlendHsv(c2, normalizedPercent).Hex()
+
 }

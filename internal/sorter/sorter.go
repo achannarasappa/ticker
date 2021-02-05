3a057f66ec621dfdd312b932cd872c047eabc947
Alexis Loiselle <alexis@t-b.ca>
2021-02-05 09:30:26 -0500

    refactor: Add functions names

--- internal/sorter/sorter.go
+++ internal/sorter/sorter.go
@@ -13,51 +13,55 @@ func NewSorter(sort string) Sorter {
 	if sorter, ok := sortDict[sort]; ok {
 		return sorter
 	} else {
-		return defaultSorter
+		return sortByChange
 	}
 }
 
 var sortDict = map[string]Sorter{
-	"alpha": func(quotes []Quote, positions map[string]Position) []Quote {
-		if len(quotes) <= 0 {
-			return quotes
-		}
-
-		result := gubrak.
-			From(quotes).
-			OrderBy(func(v Quote) string {
-				return v.Symbol
-			}).
-			Result()
-
-		return (result).([]Quote)
-	},
-	"position": func(quotes []Quote, positions map[string]Position) []Quote {
-		if len(quotes) <= 0 {
-			return quotes
-		}
-
-		activeQuotes, inactiveQuotes := splitActiveQuotes(quotes)
-
-		cActiveQuotes := gubrak.From(activeQuotes)
-		cInactiveQuotes := gubrak.From(inactiveQuotes)
-
-		positionsSorter := func(v Quote) float64 {
-			return positions[v.Symbol].Value
-		}
-
-		cActiveQuotes.OrderBy(positionsSorter, false)
-		cInactiveQuotes.OrderBy(positionsSorter, false)
-
-		result := cActiveQuotes.
-			Concat(cInactiveQuotes.Result()).
-			Result()
-
-		return (result).([]Quote)
-	},
+	"alpha":    sortByTicker,
+	"position": sortByPosition,
 }
 
-func defaultSorter(quotes []Quote, positions map[string]Position) []Quote {
+func sortByTicker(quotes []Quote, positions map[string]Position) []Quote {
+	if len(quotes) <= 0 {
+		return quotes
+	}
+
+	result := gubrak.
+		From(quotes).
+		OrderBy(func(v Quote) string {
+			return v.Symbol
+		}).
+		Result()
+
+	return (result).([]Quote)
+}
+
+func sortByPosition(quotes []Quote, positions map[string]Position) []Quote {
+	if len(quotes) <= 0 {
+		return quotes
+	}
+
+	activeQuotes, inactiveQuotes := splitActiveQuotes(quotes)
+
+	cActiveQuotes := gubrak.From(activeQuotes)
+	cInactiveQuotes := gubrak.From(inactiveQuotes)
+
+	positionsSorter := func(v Quote) float64 {
+		return positions[v.Symbol].Value
+	}
+
+	cActiveQuotes.OrderBy(positionsSorter, false)
+	cInactiveQuotes.OrderBy(positionsSorter, false)
+
+	result := cActiveQuotes.
+		Concat(cInactiveQuotes.Result()).
+		Result()
+
+	return (result).([]Quote)
+}
+
+func sortByChange(quotes []Quote, positions map[string]Position) []Quote {
 	if len(quotes) <= 0 {
 		return quotes
 	}

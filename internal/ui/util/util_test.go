8831aa784db040954cc6cef8126621d7122ff863
SnkSynthesis <63564282+SnkSynthesis@users.noreply.github.com>
2021-02-07 21:22:00 -0800

    Add more tests to make test coverage to 100%

--- internal/ui/util/util_test.go
+++ internal/ui/util/util_test.go
@@ -1,6 +1,8 @@
 package util_test
 
 import (
+	"github.com/lucasb-eyer/go-colorful"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -14,6 +16,19 @@ var _ = Describe("Util", func() {
 			Expect(output).To(Equal("12.56"))
 		})
 	})
+	Describe("ValueText", func() {
+		When("value is <= 0.0", func() {
+			It("should return an empty string", func() {
+				output := ValueText(0.0)
+				Expect(output).To(ContainSubstring(""))
+			})
+		})
+		It("should generate text for values", func() {
+			output := ValueText(435.32)
+			expectedOutput := NewStyle("#d4d4d4", "", false)("435.32")
+			Expect(output).To(Equal(expectedOutput))
+		})
+	})
 	Describe("NewStyle", func() {
 		It("should generate text with a background and foreground color", func() {
 			inputStyleFn := NewStyle("#ffffff", "#000000", false)
@@ -24,6 +39,27 @@ var _ = Describe("Util", func() {
 			inputStyleFn := NewStyle("#ffffff", "#000000", true)
 			output := inputStyleFn("test")
 			Expect(output).To(ContainSubstring("test\x1b[0m"))
+		})
+	})
+	Describe("NewStyleFromGradient", func() {
+		c1, _ := colorful.Hex("#ffffff")
+		c2, _ := colorful.Hex("#000000")
+		inputGradientFn := NewStyleFromGradient("#ffffff", "#000000")
+		When("the percent given is 100%", func() {
+			It("should generate text with the gradient of two colors relative to the percentage given", func() {
+				inputStyleFn := inputGradientFn(100)
+				output := inputStyleFn("test")
+				expectedOutput := NewStyle(c1.BlendHsv(c2, 1.0).Hex(), "", false)("test")
+				Expect(output).To(Equal(expectedOutput))
+			})
+		})
+		When("the percent given is 1%", func() {
+			It("should generate text with the gradient of two colors relative to the percentage given", func() {
+				inputStyleFn := inputGradientFn(1)
+				output := inputStyleFn("test")
+				expectedOutput := NewStyle(c1.BlendHsv(c2, 0.01).Hex(), "", false)("test")
+				Expect(output).To(Equal(expectedOutput))
+			})
 		})
 	})
 })
